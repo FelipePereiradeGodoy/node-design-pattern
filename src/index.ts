72af101b4a31d8361df282ff4a12c080dbcf80e5
FelipePereiradeGodoy <godoypereirafelipe@gmail.com>
2025-04-10 19:51:21 -0300

    feat: add pattern factory-method

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,7 @@
+console.log('============== Padrões Criacionais ==============');
+console.log()
 require('./creational/singleton/singleton');
+console.log()
+require('./creational/factory-method/factory-method');
+console.log()
+console.log('=================================================');
